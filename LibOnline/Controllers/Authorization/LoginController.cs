47193d3fb39478f8d89b35a42c732d364c548905
Igor Pereiaslovsckiy <Pereiaslovckiy.Igor@hotmail.com>
2019-02-13 12:54:01 +0300

    13.02.2019 morning

---
+++ LibOnline/Controllers/Authorization/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibOnline.Controllers.Authorization
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibOnline/Controllers/Blog/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibOnline.Controllers.Blog
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
